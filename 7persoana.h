a35333c11f3074a5cb05b16da894a0998a22bd86
gabs9116 <tofangabriela09@gmail.com>
2026-04-03 00:59:57 +0300

    Change constructor parameters to use references

--- 7persoana.h
+++ 7persoana.h
@@ -10,7 +10,7 @@ class Persoana : public Entitate {
         std::string telefon; // un sir de 10 cifre
     public:
         // constructor cu parametri
-        Persoana(std::string nume, std::string prenume, std::string telefon);
+        Persoana(std::string& nume, std::string& prenume, std::string& telefon);
 
         // destructor virtual
         virtual ~Persoana();
